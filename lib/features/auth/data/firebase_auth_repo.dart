5e1f6aeeaa6dcfdb07234eb9fb9c32d18dd4cbc7
krish lanjewar <lanjewarkrish07@gmail.com>
2025-09-12 16:11:58 +0530

    Add Firebase authentication repository and user entity implementation

---
+++ lib/features/auth/data/firebase_auth_repo.dart
@@ -0,0 +1,102 @@
+// fire base as back end
+
+import 'package:dwrl_project/features/auth/domain/entities/app_user.dart';
+import 'package:dwrl_project/features/auth/domain/repos/auth_repo.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthRepo implements AuthRepo {
+  // TODO: implement the methods using firebase auth package
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  // LOGIN : eMAIL AND PASSWORD
+  @override
+  Future<AppUser?> loginWithEmailPassword(String email, String password) async {
+    try {
+      // attempt to sign in
+      UserCredential userCredential = await firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
+      // create user from firebase user
+      AppUser user = AppUser(email: email, uid: userCredential.user!.uid);
+      return user;
+    } 
+      catch (e) {
+      throw Exception('login failed: $e');
+    
+    }
+  }
+
+  // REGISTER: EMAIL AND PASSWORD
+  @override
+  Future<AppUser?> registerWithEmailPassword(
+    String name,
+    String email,
+    String password,
+  ) async {
+    try{
+      // attempt to register
+      UserCredential userCredential = await firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      // create user from firebase user
+      AppUser user = AppUser(email: email, uid: userCredential.user!.uid);
+     
+      return user;
+    } catch (e) {
+      throw Exception('registration failed: $e');
+    }
+  }
+
+// DELETE ACCOUNT
+  @override
+  Future<void> deleteAccount() async {
+    try {
+  // get current user
+  final user = firebaseAuth.currentUser;
+
+  // check if user is null
+  if (user == null) {
+    throw Exception('No user logged in....');
+  }
+
+  // delete user
+  await user.delete();
+  await logout(); // log out after deleting account
+
+} catch (e) {
+  // handle error (e.g., re-authentication may be required)
+  print('Error deleting user: $e');
+}
+
+  }
+
+// get current user
+  @override
+  Future<AppUser?> getCurrentUser() async {
+    // get current user from firebase auth
+      final firebaseUser = firebaseAuth.currentUser;
+      // no logged in user
+      if (firebaseUser == null) return null;
+      
+      // logged in user exists 
+        return AppUser(email: firebaseUser.email!, uid: firebaseUser.uid);
+      }
+
+// LOGOUT
+  @override
+  Future<void> logout() async {
+    try {
+      await firebaseAuth.signOut();
+    } catch (e) {
+      throw Exception('Failed to log out: $e');
+    }
+  }
+// send password reset email 
+  @override
+  Future<String> sendPasswordResetEmail(String email) async {
+    try {
+     await firebaseAuth.sendPasswordResetEmail(email: email);
+      return Future.value('Password reset email sent');
+    } catch (e) {
+      throw Exception('Failed to send password reset email: $e');
+    }
+  }
+}
---
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,19 @@
+/*
+AUTH REPOSITORY - Outline the possible auth operations for this app.
+*/
+
+import 'package:dwrl_project/features/auth/domain/entities/app_user.dart';
+
+
+abstract class AuthRepo {
+  Future<AppUser?> loginWithEmailPassword(String email, String password);
+  Future<AppUser?> registerWithEmailPassword(
+    String name,
+    String email,
+    String password,
+  );
+  Future<void> logout();
+  Future<AppUser?> getCurrentUser();
+  Future<String> sendPasswordResetEmail(String email);
+  Future<void> deleteAccount();
+}
